de5d8fb5f05d5be955770bd3b40288a4579e836d
Matthieu Felix <matthieufelix@gmail.com>
2017-04-20 15:33:28 -0400

    Updated DB API

--- src/database.py
+++ src/database.py
@@ -2,6 +2,15 @@ class Database:
     """
     An image database. The database is stored on disk, and streamed as required.
     
+             
+    Usage:
+         - To use a database as input, call table_generators(), which will return a list of TableGenerator objects,
+           one per table in the database. A TableGenerator is a python generator and will sequentially return
+           every row in the table
+         - To add a column to the DB (as output), call add_column() with the name of the new column. This will
+           return a list of TableColumnWriter objects (guaranteed to be in the same order as the TableGenerators
+           returned by table_generators()), on which you can call write_row to write data into successive rows.
+    
     Storage model:
          - Video files are stored on disk as video files
          - All other data is stored as a number of individual files, one per table, in serialized form.
@@ -16,34 +25,41 @@ class Database:
         
         TODO: or should it be 100 lines per 100 lines, for instance?
         """
-        def __init__(self, vidfile, vid_id=None):
+        def __init__(self, vidfile):
             pass
 
         def __del__(self):
             pass
 
         def __next__(self):
-            """Return the next row"""
+            """Return the next row, as a NamedTuple"""
             pass
 
-    def __init__(self, directory, videos=None):
+    class TableColumnWriter:
+        def __init__(self, table, column):
+            pass
+
+        def write_row(self, data):
+            """Writes data in the next row in this column"""
+            pass
+
+    def __init__(self, directory):
         """
         :param directory: The directory in which to store any additional database data
-        :param videos: An optional list of video files to create a database from
         """
-        pass
-
-    def get_table(self, name):
-        """
-        :return: a table generator for the named video
-        """
-        return self.TableGenerator(name)
+        self.files = []
 
     def table_generators(self):
         """
         :return: a list of all table generators in the database 
         """
-        pass
+        return [self.TableGenerator(fname) for fname in self.files]
 
     def add_column(self, name):
-        pass
+        """
+        Adds a column to a database.
+        :return: A list of TableColumnGenerators.
+        """
+
+    def ingest(self, files):
+        self.files.extend(files.copy())
