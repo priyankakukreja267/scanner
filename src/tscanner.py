9e5e0fadc0614e2a6b624ae44e953317d4b20332
Priyanka Kukreja <priyankakukreja267@gmail.com>
2017-05-08 16:56:54 -0400

    Major change to tscanner

--- src/tscanner.py
+++ src/tscanner.py
@@ -1,19 +1,18 @@
-import tensorflow as tf
-import numpy as np
-from src import database
+import database, queue_manager
 
 class TScanner:
     """
     TScanner main access point
     """
 
-    def __init__(self, some_dir):
-        # self.input = some input q.deque --> represents the tensor
-        nPixels = 400 * 600
-        npArr = np.zeros(shape=(1,nPixels)) # SIZE??
-        self.input = tf.Variable(npArr, name="input")
-        self.stored_columns = list()
-        self.db = database.Database(some_dir)
+    def __init__(self, db_directory='.db'):
+        self.db = database.Database(db_directory)
+        self.qm = None
+        self.input_tensors = dict()
+        self.input_columns = None
+        self.column_tensors = dict()
+        self.column_types = dict()
+        self.output_columns = []
 
     def ingest(self, file_list):
         """
@@ -22,6 +21,12 @@ class TScanner:
         """
         self.db.ingest(file_list)
 
+    def declare_inputs(self, columns):
+        self.queue_manager = queue_manager.QueueManager(self.db, columns)
+        input_tensor = self.queue_manager.dequeue()
+        for i_c, c in enumerate(columns):
+            self.input_tensors[c] = input_tensor[i_c]
+
     def task(self, input_columns, kernel, output_columns):
         """
         Adds a task to the pipeline
@@ -29,27 +34,41 @@ class TScanner:
         :param kernel: The kernel to run
         :param output_columns: (list of strings) The columns in which to write output from the kernel
         """
-        # tensors is a dict for managing input and output data
-        self.tensors['input_columns'] = input_columns # list of input cols
-        self.qm = QueueManager(self.db, self.tensors['input_columns'])
-        self.tensors['selected_columns'] = self.qm.get_selected_columns(input_columns) # ?
-        self.tensors['output_columns'] = kernel.apply(input_columns) # list of output columns
-        
+        # Check input columns
+        kernel_input_tensors = []
+        for ic in input_columns:
+            if ic in self.input_tensors:
+                kernel_input_tensors.append(self.input_tensors[ic])
+            elif ic in self.column_tensors:
+                kernel_input_tensors.append(self.column_tensors[ic])
+            else:
+                raise Exception("Unknown column {}".format(ic))
+
+        # Check output columns
+        kernel_output_tensors = []
+        for type, name in zip(kernel.get_output_dtypes(), output_columns):
+            if name in self.column_tensors:
+                raise Exception("Column {} already defined".format(name))
+            self.column_types[name] = database.ColumnSpecification(name, video=type[0], dtype=type[1])
+
+        # Send kernel output to correct column
+        out = kernel.apply(kernel_input_tensors)
+        for k_out, col_name in zip(out, output_columns):
+            self.column_tensors[col_name] = k_out
+
     def declare_output(self, column):
         """
         Declare the named column as an output column (i.e. it will be present in the final database).
-        add to a list of output columns
         """
-        self.stored_columns.append(column)
+        self.db.add_column(self.column_types[column])
+        self.output_columns.append(column)
 
     def run(self):
         """
         Runs the computation
         :return: A database instance?
         """
-        #output = tf.tuple([self.tensors[col] for col in self.stored_columns])
-        #output.run()
-
-        inputs = self.qm.dequeue()
-        out = self.qm.enqueue(inputs, (ColumnSpecification(self.tensors['output_columns'], video=True), ColumnSpecification(self.tensors['input_columns'], dtype='float')))
-        qm.run_tensor(out)
+        output = [self.column_tensors[c] for c in self.output_columns]
+        colspecs = [self.column_types[c] for c in self.output_columns]
+        enqueuer = self.queue_manager.enqueue(output, colspecs)
+        self.queue_manager.run_tensor(enqueuer)
